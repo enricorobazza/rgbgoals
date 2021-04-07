1d252ac88b32f78372d17eadcf94357cf6c0ad4e
Enrico <enricorobazzi@gmail.com>
2021-04-07 05:17:10 -0300

    Database config

--- rgbgoals/settings.py
+++ rgbgoals/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'rgbgoals.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["DATABASE_NAME"],
+        'USER': os.environ["DATABASE_USER"],
+        'PASSWORD': os.environ["DATABASE_PASSWORD"],
+        'HOST': os.environ["DATABASE_HOST"],
+        'PORT': os.environ["DATABASE_PORT"],
     }
 }
 
@@ -132,3 +136,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ] 
+
+django_heroku.settings(locals())
